1de06f795e7fd9bbf2ecd96cb88078f34f6148c8
ismail_G <gihzoismail@gmail.com>
2023-12-25 19:02:42 +0200

    web commit

--- index.js
+++ index.js
@@ -37,11 +37,34 @@ var myclose=document.querySelector('.order');
 document.querySelector('.closee').onclick=function(){
     myclose.style.display="none";
 }
+const nextButton=document.querySelector('.next');
+const nextContent=document.querySelector('.next-content');
+nextButton.addEventListener("click",()=>{
+    if(nextContent.style.display=="none"){
+        nextContent.style.display="block";
+    }
+    else{
+        nextContent.style.display="block";
+    }
+})
 
 // document.querySelector('.submit').onclick=function(){
-// var text=document.querySelector('.text').Value;
-// if(text=="@"){
-//     document.querySelector('.label').innerHTML="you must enter valid email";
-// }
-// }
-
+// // var text=document.querySelector('.text').Value;
+// // if(text=="@"){
+// //     document.querySelector('.label').innerHTML="you must enter valid email";
+// // }
+// // }C:\Users\Ismail\Documents\my company project\images
+let content=document.querySelector('.content');
+ var slide=['images/back.jpg','images/earphone.jpg', 'images/phone.jpg'];
+ var start=0;
+  function slide(){
+    if(start<slide.length){
+        start+=1;
+    }
+    else{
+        start=1;
+    }
+    console.log(content);
+    content.innerHTML="<img src="+slide[start-1]+">";
+  }
+  setInterval(slide,2000)
